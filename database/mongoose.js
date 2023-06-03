a5b2491301b360fbfed260335d7df8949db63604
Kajol1508 <77542354+Kajol1508@users.noreply.github.com>
2023-06-03 22:15:47 +0530

    Error message implemented.

--- database/mongoose.js
+++ database/mongoose.js
@@ -4,6 +4,6 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://127.0.0.1:27017/taskManager_db')
     .then(() => { console.log("Database connected successfully!!") })
-    .catch((error) => { console.log(error) });
+    .catch((error) => { console.log("Error while Connecting database", error) });
 
 module.exports = mongoose;
