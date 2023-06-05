9f81c84b699d00821db1f423a2c14a4039aac806
Kajol1508 <77542354+Kajol1508@users.noreply.github.com>
2023-06-05 13:21:13 +0530

    CRUD operations on tasks

--- app.js
+++ app.js
@@ -97,7 +97,70 @@ app.patch('/tasklists/:tasklistId', (req, res)=>{
 app.delete('/tasklists/:tasklistId', (req, res)=>{
     TaskList.findByIdAndDelete(req.params.tasklistId)
     .then((taskList) => {
-        res.status(201).send(taskList);
+        res.status(200).send(taskList);
+    })
+    .catch((error) => {
+        console.log(error);
+        res.status(500);
+
+    });
+});
+
+// Creating tasks now
+app.get('/tasklists/:tasklistId/tasks', (req, res)=>{
+    Task.find({_taskListId: req.params.tasklistId})
+    .then((tasks) => {
+        res.status(201).send(tasks);
+    })
+    .catch((error) => {
+        console.log(error);
+        res.status(500);
+
+    });
+});
+// Create task inside tasklist
+app.post('/tasklists/:tasklistId/tasks', (req, res) => {
+    console.log(req.body)
+    let taskObject = { 'title': req.body.title, '_taskListId': req.params.tasklistId };
+    Task(taskObject).save()
+        .then((task) => {
+            res.status(201).send(task);
+        })
+        .catch((error) => {
+            res.status(500);
+            console.log(error);
+        });
+});
+
+
+app.get('/tasklists/:tasklistId/tasks/:taskId', (req, res)=>{
+    Task.findOne({_taskListId: req.params.tasklistId, _id: req.params.taskId})
+    .then((task) => {
+        res.status(201).send(task);
+    })
+    .catch((error) => {
+        console.log(error);
+        res.status(500);
+
+    });
+});
+
+app.patch('/tasklists/:tasklistId/tasks/:taskId', (req, res)=>{
+    Task.findOneAndUpdate({_taskListId: req.params.tasklistId, _id: req.params.taskId}, {$set: req.body})
+    .then((task) => {
+        res.status(200).send(task);
+    })
+    .catch((error) => {
+        console.log(error);
+        res.status(500);
+
+    });
+});
+
+app.delete('/tasklists/:tasklistId/tasks/:taskId', (req, res)=>{
+    Task.findByIdAndDelete(req.params.taskId)
+    .then((task) => {
+        res.status(200).send(task);
     })
     .catch((error) => {
         console.log(error);
@@ -107,7 +170,6 @@ app.delete('/tasklists/:tasklistId', (req, res)=>{
 });
 
 // Using Arrow function
-
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
--- database/models/task.js
+++ database/models/task.js
@@ -13,8 +13,7 @@ const TaskSchema = new mongoose.Schema({
     },
     completed: {
         type: Boolean,
-        default: false,
-        required: true
+        default: false
     }
 });
 
