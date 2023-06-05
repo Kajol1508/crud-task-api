50e0c1ee77d0c375b213b7a074eda4e2498df19b
Kajol1508 <77542354+Kajol1508@users.noreply.github.com>
2023-06-05 11:16:06 +0530

    added update and delete routes using postman

--- app.js
+++ app.js
@@ -1,13 +1,113 @@
 const express = require('express');
 const app = express();
 
-const mongoose = require('./database/mongoose')
+const mongoose = require('./database/mongoose');
 
-// app.listen(3000, function(){
-//     console.log("Server started on port 3000");
-// });
+const TaskList = require('./database/models/taskList');
+const Task = require('./database/models/task');
+
+
+app.use((req, res, next) => {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+app.use(express.json());
+
+// Routes of Rest API
+/**
+ * TaskList- Create, Update, ReadTaskListById, ReadAllTaskList
+ * Task- Create, Update, ReadTaskById, ReadAllTask
+ * 
+ */
+
+app.get('/tasklists', (req, res) => {
+    TaskList.find({})
+        .then((lists) => {
+            res.status(200).send(lists);
+
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500);
+        });
+});
+app.get('/tasklists/:tasklistId', (req, res) => {
+    let tasklistId = req.params.tasklistId;
+    TaskList.find({ _id: tasklistId })
+        .then((taskList) => {
+            res.status(200).send(taskList);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500);
+
+        });
+});
+
+app.post('/tasklists', (req, res) => {
+    console.log(req.body)
+    let taskListObject = { 'title': req.body.title };
+    TaskList(taskListObject).save()
+        .then((taskList) => {
+            res.status(201).send(taskList);
+        })
+        .catch((error) => {
+            res.status(500);
+            console.log(error);
+        });
+});
+// Full Update of object
+app.put('/tasklists/:tasklistId', (req, res)=>{
+    TaskList.findOneAndUpdate({_id: req.params.tasklistId}, {$set: req.body})
+    .then((taskList) => {
+        res.status(200).send(taskList);
+    })
+    .catch((error) => {
+        console.log(error);
+        res.status(500);
+
+    });
+});
+// update only one field of an object
+app.patch('/tasklists/:tasklistId', (req, res)=>{
+    TaskList.findOneAndUpdate({_id: req.params.tasklistId}, {$set: req.body})
+    .then((taskList) => {
+        res.status(200).send(taskList);
+    })
+    .catch((error) => {
+        console.log(error);
+        res.status(500);
+
+    });
+});
+
+app.delete('/tasklists/:tasklistId', (req, res)=>{
+    TaskList.findByIdAndDelete(req.params.tasklistId)
+    .then((taskList) => {
+        res.status(201).send(taskList);
+    })
+    .catch((error) => {
+        console.log(error);
+        res.status(500);
+
+    });
+});
 
 // Using Arrow function
-app.listen(3000, ()=>{
+
+app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
---
+++ database/models/taskList.js
@@ -0,0 +1,18 @@
+// for getting the data from the database
+
+const mongoose = require('mongoose');
+const TaskListSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        trim: true,
+        minlength: 3
+    },
+    // name:{
+    //     type: String,
+    //     trim: true,
+    //     minlength: 3
+    // }
+});
+
+const TaskList = mongoose.model('TaskList', TaskListSchema);
+module.exports = TaskList;
